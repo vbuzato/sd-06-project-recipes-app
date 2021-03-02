55909d66479ca467ca6bb1c05fe5de4176e459be
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-03-02 00:22:15 -0300

    Updates

---
+++ precache-manifest.edd19740acd3bb6184ea50e329226278.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e55420f774e994afb0518a2454aef4d",
+    "url": "/sd-06-project-recipes-app/index.html"
+  },
+  {
+    "revision": "698d84d1bde06b86d4f7",
+    "url": "/sd-06-project-recipes-app/static/css/main.6be78cb8.chunk.css"
+  },
+  {
+    "revision": "1f134835485db43fec37",
+    "url": "/sd-06-project-recipes-app/static/js/2.ec6833e8.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/sd-06-project-recipes-app/static/js/2.ec6833e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698d84d1bde06b86d4f7",
+    "url": "/sd-06-project-recipes-app/static/js/main.ac17edeb.chunk.js"
+  },
+  {
+    "revision": "a5e13bc9536e463825b8",
+    "url": "/sd-06-project-recipes-app/static/js/runtime-main.d10a7bcc.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/sd-06-project-recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/sd-06-project-recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/sd-06-project-recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "383547711c11d56fab399d2161db41b6",
+    "url": "/sd-06-project-recipes-app/static/media/loader.38354771.gif"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/sd-06-project-recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "3ad22b947a1132d1cf2038e411e94575",
+    "url": "/sd-06-project-recipes-app/static/media/mexican-food.3ad22b94.jpg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/sd-06-project-recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/sd-06-project-recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/sd-06-project-recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/sd-06-project-recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
